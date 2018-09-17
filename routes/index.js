e1a8651040d3d6e0ca7dcadcdde412bb82303de7
Matt Rencher <mattrencher@gmail.com>
2018-09-17 20:52:30 +0000

    fix mailgun api key

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,7 @@ var express = require("express"),
 
 // Mailgun variables
 var domain = 'mg.renchtech.com';
-var apiKey = "***REMOVED***";
-var mailgun = require('mailgun-js')({apiKey: process.env.mg_api_key || apiKey, domain: domain});
+var mailgun = require('mailgun-js')({apiKey: process.env.mg_api_key || process.env.apiKey, domain: domain});
 
 // Root Route
 router.get("/", function(req, res){
